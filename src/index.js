fcc5b20b467dba34bd03896e9a47ab18d066b3d6
René Meye <rene@vesputi.com>
2021-02-01 21:04:23 +0100

    refactor: switches to use axios and have a running integration test

--- src/index.js
+++ src/index.js
@@ -1,52 +1,127 @@
+const axios = require('axios');
+
+/**
+ * The main class to interact with the The Mobilitybox API.
+ */
 class Mobilitybox {
+  /**
+   * Create the API access object with your API key
+   * @param {String} access_token You API Access Token
+   * @param {String} base_url The base URL used (optional)
+   */
   constructor(access_token, base_url = "https://api.themobilitybox.com/v1") {
     this.access_token = access_token;
     this.base_url = base_url;
   }
 
+  /**
+   * This callback gets stations from an async function.
+   * @callback stationCallback
+   * @param {MobilityboxStation[]} stations
+   */
+
+  /**
+   * Find stations by name
+   * @param {String} query The query to use for station name search
+   * @param {stationCallback} callback A callback receiving the stations
+   */
   find_stations_by_name(query, callback){
-    fetch(this.base_url+'/stations/search_by_name.json?query='+query)
-      .then(response => response.json())
-      .then(data => data.map((station_data)=> new MobilityboxStation(station_data, this)))
-      .then(callback)
+    axios.get(this.base_url+'/stations/search_by_name.json?query='+query)
+      .then(response => callback(response.data.map((station_data)=> new MobilityboxStation(station_data, this))))
   }
 
+  /**
+   * Find stations by positions
+   * @param {Object} position An object containing latitude and longitude for positions
+   * @param {stationCallback} callback A callback receiving the stations
+   */
   find_stations_by_position(position, callback){
-    fetch(this.base_url+'/stations/search_by_position.json?latitude='+position.latitude+'&longitude='+position.longitude)
-      .then(response => response.json())
-      .then(data => data.map((station_data)=> new MobilityboxStation(station_data, this)))
-      .then(callback)
+    axios.get(this.base_url+'/stations/search_by_position.json?latitude='+position.latitude+'&longitude='+position.longitude)
+      .then(response => callback(response.data.map((station_data)=> new MobilityboxStation(station_data, this))))
   }
 
+  /**
+   * @typedef AttributionsHash
+   * @type {object}
+   * @property {string} html - The html-version
+   * @property {string} text - The text-only version
+   * @property {string} url - The URL to link to
+  */
+
+  /**
+   * This callback gets an object with an HTML/text-only version of
+   * @callback attributionsCallback
+   * @param {AttributionsHash} attributions
+   */
+
+   /**
+    * Get the attribution text and url to use in your app
+    * @param {attributionsCallback} callback A callback receiving the attributions as HTML, text and a URL
+    */
   get_attributions(callback){
-    fetch(this.base_url+'/attributions.json')
-      .then(response => response.json())
-      .then(callback)
+    axios.get(this.base_url+'/attributions.json')
+      .then(response => callback(response.data))
   }
 
+  /**
+   * This callback receives a trip.
+   * @callback tripCallback
+   * @param {MobilityboxTrip} attributions
+   */
+
+   /**
+    * Get a trip by ID
+    * @param {String} trip_id The Trip ID
+    * @param {tripCallback} callback A callback receiving a single trip
+    */
   get_trip(trip_id, callback){
-    fetch(this.base_url+'/trips/'+trip_id+'.json')
-      .then(response => response.json())
-      .then((trip_data) => new MobilityboxTrip(trip_data, this))
-      .then(callback)
+    axios.get(this.base_url+'/trips/'+trip_id+'.json')
+      .then(response => callback(new MobilityboxTrip(response.data, this)))
   }
 }
 
+/**
+ * The class representing a single Station
+ */
 class MobilityboxStation {
+  /**
+   * @typedef StationDataHash
+   * @type {object}
+   * @property {string} id - Station ID
+   * @property {string} name - Name of the Station
+   */
+
+  /**
+   * Create a station from raw data
+   * @param {StationDataHash} station_data Raw Station data
+   * @param {Mobilitybox} mobilitybox Mobilitybox object (the main API object)
+   */
   constructor(station_data, mobilitybox) {
     this.id = station_data.id;
     this.name = station_data.name;
     this.mobilitybox = mobilitybox;
   }
 
+  /**
+   * This callback receives departures
+   * @callback departuresCallback
+   * @param {MobilityboxDeparture[]} departures
+   */
+
+  /**
+   * Fetch Next Departures for this Station
+   * @param {departuresCallback} callback A callback receiving departures for this station and time
+   * @param {Date} time A time to receive departures for (defaults to now)
+   */
   get_next_departures(callback, time = Date.now()) {
-    fetch(this.mobilitybox.base_url+'/departures.json?station_id='+this.id+'&time='+time)
-      .then(response => response.json())
-      .then(data => data.map((station_data)=> new MobilityboxDeparture(station_data, this.mobilitybox)))
-      .then(callback)
+    axios.get(this.mobilitybox.base_url+'/departures.json?station_id='+this.id+'&time='+time)
+      .then(response => callback(response.data.map((station_data)=> new MobilityboxDeparture(station_data, this.mobilitybox))))
   }
 }
 
+/**
+ * The class representing a Departure
+ */
 class MobilityboxDeparture {
   constructor(departure_parameters, mobilitybox) {
     this.mobilitybox = mobilitybox;
@@ -89,7 +164,23 @@ class MobilityboxEventTime {
   }
 }
 
+/**
+ * This class represents a single trip.
+ */
 class MobilityboxTrip {
+  /**
+   * @typedef TripsHash
+   * @type {object}
+   * @property {string} id - Trip ID
+   * @property {string} name - Name of the strip
+   * @property {Array} stops - Raw stops data
+   */
+
+  /**
+   * Create a trip from raw data
+   * @param {TripsHash} trip_parameters Raw Trip Parameters
+   * @param {Mobilitybox} mobilitybox Mobilitybox object (the main API object)
+   */
   constructor(trip_parameters, mobilitybox) {
     this.mobilitybox = mobilitybox;
 
@@ -98,6 +189,11 @@ class MobilityboxTrip {
     this.stops = trip_parameters.stops.map((stop_data)=> new MobilityboxStop(stop_data, this.mobilitybox));
   }
 
+  /**
+   * Receive a human-readable version of this trip.
+   *
+   * @return {String} Human-readable String representation
+   */
   date_formated(){
     var start_date_formated = this.stops[0].departure.scheduled_at_date_formated();
     var end_date_formated = this.stops[this.stops.length-1].arrival.scheduled_at_date_formated();
@@ -109,15 +205,28 @@ class MobilityboxTrip {
     }
   }
 
+  /**
+   * Receive the starting station for this trip.
+   *
+   * @return {MobilityboxStation} Startin Station
+   */
   from_station(){
     return this.stops[0].station;
   }
 
+  /**
+   * Receive the final destination station for this trip.
+   *
+   * @return {MobilityboxStation} Destination station
+   */
   to_station(){
     return this.stops[this.stops.length-1].station;
   }
 }
 
+/**
+ * The class for a single stop in a trip
+ */
 class MobilityboxStop {
   constructor(stop_parameters, mobilitybox) {
     this.mobilitybox = mobilitybox;
@@ -128,3 +237,11 @@ class MobilityboxStop {
     this.arrival = new MobilityboxEventTime(stop_parameters.arrival);
   }
 }
+
+module.exports = {
+  Mobilitybox: Mobilitybox,
+  MobilityboxDeparture: MobilityboxDeparture,
+  MobilityboxStop: MobilityboxStop,
+  MobilityboxEventTime: MobilityboxEventTime,
+  MobilityboxTrip: MobilityboxEventTime
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,47 @@
-import Mobilitybox from './index.js'
-import {expect} from 'chai';
+import { Mobilitybox } from './index.js'
+import { expect } from 'chai';
 
 describe('Mobilitybox', ()=>{
-  it('should work', ()=>{
-    expect(true).to.be.true;
+  it('initializes with an api token', ()=>{
+    const mobilitybox = new Mobilitybox('abc');
+    expect(mobilitybox.access_token).to.eq("abc");
   });
+
+  it('can initialize without a base url', ()=>{
+    const mobilitybox = new Mobilitybox('abc');
+    expect(mobilitybox.base_url).to.eq("https://api.themobilitybox.com/v1");
+  });
+
+  it('can initialize with a given base url', ()=>{
+    const mobilitybox = new Mobilitybox('abc', 'https://foobar.lol/v42');
+    expect(mobilitybox.base_url).to.eq("https://foobar.lol/v42");
+  });
+});
+
+describe( 'Integration', ()=>{
+  it('runs the integration code', ()=>{
+    const mobilitybox_access_token = 'hallo_welt123';
+    const mobilitybox = new Mobilitybox(mobilitybox_access_token);
+    mobilitybox.get_attributions((attributions)=>{
+      console.log('=== Output of Integration Test ===');
+      console.log('Attributions: '+attributions.text);
+    })
+
+    mobilitybox.find_stations_by_name("Hamburg-Dammtor", (stations)=>{
+      var station = stations[0];
+
+      console.log('Next Departures for Station: '+station.name);
+
+      station.get_next_departures((departures)=>{
+        departures.map((departure)=>{
+          console.log(
+            " - ",
+            departure.departure_time.scheduled_at_formated(),
+            departure.line_name,
+            departure.headsign
+          )
+        })
+      });
+    });
+  })
 });
